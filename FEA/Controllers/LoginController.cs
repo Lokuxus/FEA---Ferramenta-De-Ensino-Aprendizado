a6673675980c603533c43a9f3834568cb6f68540
AlexsandroTC <alexsandro.tcunha@hotmail.com>
2017-03-06 20:21:43 -0300

    commit

---
+++ FEA/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FEA.Models;
+
+namespace FEA.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            var usuario = new UsuarioViewModel();
+            return View("Login", usuario);
+        }
+
+        [HttpPost]
+        public ActionResult Logar(UsuarioViewModel usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", usuario);
+            }
+
+            return View("Login", usuario);
+        }
+
+        private ActionResult Resultado(UsuarioViewModel usuario)
+        {
+            return View(usuario);
+        }
+
+    }
+}
---
+++ FEA/Models/UsuarioViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FEA.Models
+{
+    public class UsuarioViewModel
+    {
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Senha { get; set; }
+    }
+}
