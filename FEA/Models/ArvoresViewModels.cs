e00ba2b83cb49e4c225a7c4fa9f008737577434d
Lokuxus <3768210@univali.br>
2017-03-20 21:41:29 -0300

    Testando views

---
+++ FEA/Models/ArvoresViewModels.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FEA.Models
+{
+    public class CadastroArvoreViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Matéria")]
+        public string Materia { get; set; }
+    }
+
+}
