c3302f669e4990fd1de242b0b4a583929cf492dc
AlexsandroTC <alexsandro.tcunha@hotmail.com>
2017-03-24 22:00:51 -0300

    Comit Cadastro de Questoes

---
+++ FEA/Models/RegisterViewModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace FEA.Models
+{
+    public class RegisterQuestaoViewModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Questao")]
+        public string Questao { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Resposta")]
+        public string Resposta { get; set; }
+
+    }
+}
